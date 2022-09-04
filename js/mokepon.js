76ef77ebc63b3efb971cf17aedc80746be4a332a
Adhemar <adhemarduran@gmail.com>
2022-09-04 10:06:48 -0400

    select enemy's pet randomly

--- js/mokepon.js
+++ js/mokepon.js
@@ -31,6 +31,41 @@ function selectPlayerPet() {
     }
 
     spanPlayerPet.innerHTML = pet;
+
+    selectEnemyPet();
+}
+
+function selectEnemyPet() {
+    let randomPetNumber = random(1, 6);
+    let spanEnemyPet = document.getElementById('enemy-pet');
+
+    switch (randomPetNumber) {
+        case 1:
+            spanEnemyPet.innerHTML = "Hipodoge";
+            break;
+        case 2:
+            spanEnemyPet.innerHTML = "Capipepo";
+            break;
+        case 3:
+            spanEnemyPet.innerHTML = "Ratigueya";
+            break;
+        case 4:
+            spanEnemyPet.innerHTML = "Langostelvis";
+            break;
+        case 5:
+            spanEnemyPet.innerHTML = "Tucapalma";
+            break;
+        case 6:
+            spanEnemyPet.innerHTML = "Pydos";
+            break;
+        default:
+            alert("There was an error selecting enemy's pet");
+            break;
+    }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min +1) + min)
 }
 
 
